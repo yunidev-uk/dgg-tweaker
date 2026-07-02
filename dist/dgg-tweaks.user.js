d6f78a0a1b8b6500d6d7c6ca6117c56434abcb9d
Alex Davies <alex@frogtrade.com>
2026-07-02 21:26:29 +0100

    Bump version to 2.1.0

--- dist/dgg-tweaks.user.js
+++ dist/dgg-tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DGG Tweaks
 // @namespace    yuniDev.dgg-tweaks
-// @version      2.0.3
+// @version      2.1.0
 // @description  UI Tweaks for destiny.gg
 // @author       yuniDev
 // @license      MIT
